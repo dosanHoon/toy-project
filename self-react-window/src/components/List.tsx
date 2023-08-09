5813e9279b0b4ccf3c938de749279347ec94f588
dosanhoon <hangru1106@gmail.com>
2023-08-09 19:16:28 +0900

    scroll 크기를 이용한 1차 구현

--- self-react-window/src/components/List.tsx
+++ self-react-window/src/components/List.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef } from "react";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
 import { ListItem } from "./ListItem";
 import { styled } from "styled-components";
 
@@ -9,19 +9,56 @@ interface ListProps {
 export const List: FC<ListProps> = ({ items }) => {
   const listRef = useRef<HTMLUListElement>(null);
   const viewPortRef = useRef<HTMLDivElement>(null);
+
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(0);
+
+  const getIndex = useCallback(
+    (scrollTop: number, listHeight: number) => {
+      const startIndex = Math.floor((scrollTop / listHeight) * items.length);
+      const endIndex = Math.floor(
+        ((scrollTop + window.innerHeight) / listHeight) * items.length
+      );
+      return [startIndex, endIndex];
+    },
+    [items]
+  );
+
   useEffect(() => {
     if (listRef.current) {
-      listRef.current.style.height = listRef.current.clientHeight + "px";
+      listRef.current.style.height = 200 * items.length + "px";
     }
-  }, []);
+  }, [items]);
+
+  useEffect(() => {
+    const viewPort = viewPortRef.current;
+    const listCurrent = listRef.current;
+    if (viewPort) {
+      viewPort.addEventListener("scroll", () => {
+        if (listCurrent && viewPort) {
+          const scrollTop = viewPort.scrollTop;
+          const listHeight = listCurrent.clientHeight;
+
+          const [startIndex, endIndex] = getIndex(scrollTop, listHeight);
+          setStartIndex(startIndex);
+          setEndIndex(endIndex);
+        }
+      });
+    }
+  }, [viewPortRef, listRef, items, setStartIndex, setEndIndex, getIndex]);
+
   return (
     <Container ref={viewPortRef}>
       <StyledList ref={listRef}>
-        {items.map((item) => (
-          <ListItem key={item} viewPortRef={viewPortRef}>
-            {item}
-          </ListItem>
-        ))}
+        {items.map(
+          (item, index) =>
+            index >= startIndex &&
+            index <= endIndex && (
+              <ListItem key={item} viewPortRef={viewPortRef}>
+                {item}
+              </ListItem>
+            )
+        )}
       </StyledList>
     </Container>
   );
